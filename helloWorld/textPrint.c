b0720c171ef7441d76ddc56aea0304f92d3b6846
Your Name <vladislavlazdan@gmail.com>
2025-10-25 15:54:56 +0300

    fopen aded

--- helloWorld/textPrint.c
+++ helloWorld/textPrint.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    
+    FILE *text = fopen("text.txt", "r");
+    if (file == NULL) {
+        printf("Error: couldn't open file.");
+        return 1;
+    }
 }
