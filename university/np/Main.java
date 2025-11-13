d97da8171a0a65b8cb561bf305ea2aaf30d1c852
Your Name <vladislavlazdan@gmail.com>
2025-11-13 13:46:58 +0200

    fully implemented del add and print in np

--- university/np/Main.java
+++ university/np/Main.java
@@ -1,5 +1,7 @@
 package university.np;
 
+// 251RDB010 Vladislavs Lazdāns 15. grupa
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -9,32 +11,14 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
+// TODO check whole prog on file path
+
 public class Main {
-    public class travelData {
-        String city;
-        String date;
-        //TODO specify this
-        int number1;
-        int number2;
-        String vehicle;
-
-        // Konstruktors (lai ērti izveidotu objektu)
-        public travelData(String name, String surname, int grade) {
-            this.city = city;
-            this.date = date;
-            this.number1 = number1;
-            this.number2 = number2;
-            this.vehicle = vehicle;
-        }
-        
-        public String toString() {
-            return city + ";" + date + ";" + number1 + ";" + number2 + ";" + vehicle;
-        }
-    }
-
     public static Scanner sc;
+
     public static void main(String[] args) {
         sc = new Scanner(System.in);
+
 		String cmd = "";
 
 		while (!cmd.equals("exit")) {
@@ -43,23 +27,40 @@ public class Main {
 
 			switch (cmd) {
                 case "add":
-                    // TODO add correct format
-                    System.out.println("Print a line, should be formated XXX:");
-                    String line = sc.nextLine();
-                    add(line);
+                    String data = sc.next();
+                    add(data);
                     break;
-                    
-				case "exit":
-					System.out.println("good bye");
-					break;
 
                 case "del":
-                    del();
+                    int id = sc.nextInt(); 
+                    del(id);
+                    break;
+                
+                case "edit":
+                    data = sc.next();
+                    //edit(data);
                     break;
 
                 case "print":
                     print();
                     break;
+                
+                case "sort":
+                    //sort();
+                    break;
+                
+                case "find":
+                    int price = sc.nextInt();
+                    //find(price);
+                    break;
+                
+                case "avg":
+                    //avg();
+                    break;
+
+                case "exit":
+					System.out.println("good bye");
+					break;
 
 				default:
 					System.out.println("unknown command");
@@ -70,35 +71,71 @@ public class Main {
 		sc.close();
     }
 
-    public static void del() {
+    public static void del(int id) {
         // TODO before sending chande just on db.csv
-        String file = "/home/vlad/git-practice/university/np/db.csv"; 
-        System.out.print("Select line: ");
-        int lineNum = sc.nextInt();
+        String file = "/home/vlad/git-practice/university/np/db.csv";
 
-        // Load all lines from CSV file into a list
         List<String> lines = new ArrayList<>();
 
-        // read this lines
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-            String currentLine;
-            while ((currentLine = br.readLine()) != null) {
-                lines.add(currentLine); // add every line to list
-            }
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
+        lines = readFile(file);
+
+        // System.out.println(e.getMessage()); alredy implemented in readFile
+        if (lines == null) {
             return;
         }
 
-        // check if line exist and lineNum positive
-        if (lineNum <= 0 || lineNum > lines.size()) {
-            System.out.println("Line does not exist!");
-            return; // exit method if invalid number
+        if (id - 100 < 0) {
+            System.out.println("wrong id");
+            return;
+        }
+
+        int lineNum;
+
+        if ((lineNum = getLineNum(id, lines)) == -1) {
+            System.out.println("wrong id");
+            return;
         }
 
         // removes line
         lines.remove(lineNum - 1);
 
+        rewriteFile(lines, file);
+
+        System.out.println("deleted");
+    }
+
+    public static int getLineNum(int id, List<String> lines) {
+        int lineNum = 1;
+
+        for (String line : lines) {
+            String[] parts = line.split(";");
+            int lineId = Integer.parseInt(parts[0]);
+
+            if (lineId == id) {
+                return lineNum;
+            }
+
+            ++lineNum;
+        }
+
+        return -1;
+    }
+
+    public static List<String> readFile(String file) {
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String currentLine;
+            while ((currentLine = br.readLine()) != null) {
+                lines.add(currentLine);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+        return lines;
+    }
+
+    public static void rewriteFile(List<String> lines, String file) {
         // rewrites file by using lines where there is no lineNum line
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
             // Write all remaining lines back to the file
@@ -115,41 +152,150 @@ public class Main {
     public static void add(String line) {
         // TODO before sending chande just on db.csv
         String file = "/home/vlad/git-practice/university/np/db.csv"; 
+
+        String[] parts = line.split(";");
+
+        int id = Integer.parseInt(parts[0]);
+        String city = parts[1];
+        String date = parts[2];
+        int days = Integer.parseInt(parts[3]);
+        double price = Double.parseDouble(parts[4]);
+        String vehicle = parts[5];
+
         // after check on the style print new line in db.csv
-        if (checkAdd(line)) {
+        if (checkLine(line)) {
             try (FileWriter writer = new FileWriter(file, true)) {
-                writer.append(line);
+                writer.append(id + ";");
+
+                String[] words = city.split(" ");
+                StringBuilder formatted = new StringBuilder();
+
+                for (String word : words) {
+                    if (!word.isEmpty()) {
+                        formatted.append(Character.toUpperCase(word.charAt(0)))
+                             .append(word.substring(1))
+                             .append(" ");
+                    }
+                }
+
+                city = formatted.toString().trim();
+
+                writer.append(city.toLowerCase() + ";");
+                writer.append(date + ";");
+                writer.append(days + ";");
+                writer.append(price + ";");
+                writer.append(vehicle.toUpperCase());
                 writer.append("\n");
+
+                System.out.println("added");
             } catch (IOException e) {
-                // Handle any file-related errors
-                e.printStackTrace();
+                System.out.println(e.getMessage());
             }
-        } else {
-            // TODO print correct style
-            System.out.println("Incorrect line style, should be: XXX");
-            return;
         }
     }
     
-    // TODO check if the line have correct style
-    public static boolean checkAdd(String line) {
+    public static boolean checkLine(String line) {
+        String[] parts = line.split(";");
+
+        // TODO before sending chande just on db.csv
+        String file = "/home/vlad/git-practice/university/np/db.csv";
+
+        try {
+            Integer.parseInt(parts[3]);
+        } catch (NumberFormatException e) {
+            System.out.println("wrong day count");
+            return false;
+        }
+
+        try {
+            Double.parseDouble(parts[4]);
+        } catch (NumberFormatException e) {
+            System.out.println("wrong price");
+            return false;
+        }
+
+        List<String> lines = new ArrayList<>();
+        lines = readFile(file);
+
+        int id = Integer.parseInt(parts[0]);
+        String date = parts[2];
+        String vehicle = parts[5].toUpperCase();
+
+        if (parts.length != 6) {
+            System.out.println("wrong field count");
+            return false;
+        }
+
+        if (id - 100 < 0) {
+            System.out.println("wrong id");
+            return false;
+        } else if (getLineNum(id, lines) != -1) {
+            System.out.println("wrong id");
+            return false;
+        }
+        
+        String[] partsDate = date.split("/");
+
+        int dayDate = Integer.parseInt(partsDate[0]);
+        int monthDate = Integer.parseInt(partsDate[1]);
+
+        if (dayDate < 1 || dayDate > 31) { 
+            System.out.println("wrong date");
+            return false;
+        } else if (monthDate < 1 || monthDate > 12) {
+            System.out.println("wrong date");
+            return false;
+        }
+
+        if (!vehicle.equals("TRAIN") && !vehicle.equals("PLANE") && 
+                            !vehicle.equals("BUS") && !vehicle.equals("BOAT")) {
+            System.out.println("wrong vehicle");
+            return false;
+        }
+
         return true;
     }
 
     public static void print() {
         // TODO before sending chande just on db.csv
         String file = "/home/vlad/git-practice/university/np/db.csv";
-         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-            String currentLine;
-            int lineNum = 1;
-            
-            // print each line
-            while ((currentLine = br.readLine()) != null) {
-                System.out.println(lineNum + ": " + currentLine);
-                lineNum++;
-            }
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
+        int symbCount = 60;
+        int idSymbCount = 4;
+        int citySymbCount = 21;
+        int dateSymbCount = 11;
+        int daysSymbCount = 6;
+        int priceSymbCount = 10;
+        int vehicleSymbCount = 8;
+
+        
+        System.out.println("-".repeat(symbCount));
+        printLine(idSymbCount, citySymbCount, dateSymbCount, daysSymbCount, priceSymbCount, vehicleSymbCount,
+                  "ID", "City", "Date", "Days", "Price", "Vehicle");
+        System.out.println("-".repeat(symbCount));
+
+        List<String> lines = new ArrayList<>();
+        lines = readFile(file);
+
+        for (String line : lines) {
+            String[] parts = line.split(";");
+            printLine(idSymbCount, citySymbCount, dateSymbCount, daysSymbCount, priceSymbCount, vehicleSymbCount,
+                      parts[0], parts[1], parts[2], parts[3], parts[4], parts[5]);
         }
+
+        System.out.println("-".repeat(symbCount));
+    }
+
+    public static void printLine(int idSymbCount, int citySymbCount, int dateSymbCount,
+                                 int daysSymbCount, int priceSymbCount, int vehicleSymbCount,
+                                 String id, String city, String date, String days, String price, String vehicle) {
+
+        String statusLine = id + " ".repeat(idSymbCount-id.length()) + 
+                            city + " ".repeat(citySymbCount-city.length()) + 
+                            date + " ".repeat(dateSymbCount-date.length()) + 
+                            " ".repeat(daysSymbCount-days.length()) + days + 
+                            " ".repeat(priceSymbCount-price.length()) + price + 
+                            " ".repeat(vehicleSymbCount-vehicle.length()) + vehicle;
+                            
+        System.out.println(statusLine);
     }
 }
